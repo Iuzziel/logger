18d22f2971144c775c3893cf11333ba32a063d3e
iuzziel <iuzziel@hotmail.fr>
2018-12-12 20:06:38 +0100

    adding a changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,26 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+### Added
+- Changelog file.
+- Version navigation.
+- Links to latest released version in previous versions.
+
+### Changed
+- Indent with 4 spaces in typescript.
+- Version of dependence @types/node to ^10.12.12.
+- Version of dependence typescript to ^3.2.2.
+
+## [1.0.0] - 2018-12-11
+### Added
+- Singleton Logger class with four level of log.
+- Debug level of log, wich show on demand.
+- Log to the standard output.
+- Append the log to a textfile.
+
+[HEAD]: [master branch](https://github.com/Iuzziel/logger/tree/master)
+[1.0.0]: [github.com/Iuzziel/logger/releases/tag/v1.0.0](https://github.com/Iuzziel/logger/releases/tag/v1.0.0)
--- src/logger.ts
+++ src/logger.ts
@@ -1,80 +1,80 @@
 import { appendFileSync } from "fs";
 
 export class Logger {
-  private logFile: string = "undefined.log";
-  private debugEnable: boolean = false;
-  static _instance: Logger;
+    private logFile: string = "undefined.log";
+    private debugEnable: boolean = false;
+    static _instance: Logger;
 
-  constructor(loggerConfig?: LoggerConfig) {
-    if (Logger._instance) {
-      throw new Error("Singleton, utiliser Logger.getInstance()");
+    constructor(loggerConfig?: LoggerConfig) {
+        if (Logger._instance) {
+            throw new Error("Singleton, utiliser Logger.getInstance()");
+        }
+        if (loggerConfig) {
+            if (loggerConfig.debugEnable) {
+                this.debugEnable = loggerConfig.debugEnable;
+            }
+            if (loggerConfig.logFile) {
+                this.logFile = loggerConfig.logFile;
+            }
+        }
     }
-    if (loggerConfig) {
-      if (loggerConfig.debugEnable) {
-        this.debugEnable = loggerConfig.debugEnable;
-      }
-      if (loggerConfig.logFile) {
-        this.logFile = loggerConfig.logFile;
-      }
+
+    private _log(logType: LogType, message: any): void {
+        let logMessage = JSON.stringify(message);
+        appendFileSync(
+            this.logFile,
+            `[${new Date().toLocaleString()}][${logType}] ${logMessage}\r\n`
+        );
+        if (logType == LogType.INFO || logType == LogType.DEBUG) {
+            console.log(logMessage);
+        } else if (logType == LogType.ERROR || logType == LogType.FATAL) {
+            console.error(logMessage);
+        }
     }
-  }
 
-  private _log(logType: LogType, message: any): void {
-    let logMessage = JSON.stringify(message);
-    appendFileSync(
-      this.logFile,
-      `[${new Date().toLocaleString()}][${logType}] ${logMessage}\r\n`
-    );
-    if (logType == LogType.INFO || logType == LogType.DEBUG) {
-      console.log(logMessage);
-    } else if (logType == LogType.ERROR || logType == LogType.FATAL) {
-      console.error(logMessage);
+    public info(message: any): void {
+        this._log(LogType.INFO, message);
     }
-  }
 
-  public info(message: any): void {
-    this._log(LogType.INFO, message);
-  }
-
-  public error(message: any): void {
-    this._log(LogType.ERROR, message);
-  }
-
-  public fatal(message: any): void {
-    this._log(LogType.FATAL, message);
-  }
-
-  public debug(message: any): void {
-    if (this.debugEnable) {
-      this._log(LogType.DEBUG, message);
+    public error(message: any): void {
+        this._log(LogType.ERROR, message);
     }
-  }
 
-  public getLogFileName(): string {
-    return this.logFile;
-  }
-
-  public isDebug(): boolean {
-    return this.debugEnable;
-  }
-
-  static getInstance(): Logger {
-    if (!Logger._instance) {
-      Logger._instance = new Logger();
+    public fatal(message: any): void {
+        this._log(LogType.FATAL, message);
     }
-    return Logger._instance;
-  }
+
+    public debug(message: any): void {
+        if (this.debugEnable) {
+            this._log(LogType.DEBUG, message);
+        }
+    }
+
+    public getLogFileName(): string {
+        return this.logFile;
+    }
+
+    public isDebug(): boolean {
+        return this.debugEnable;
+    }
+
+    static getInstance(): Logger {
+        if (!Logger._instance) {
+            Logger._instance = new Logger();
+        }
+        return Logger._instance;
+    }
 }
 
 enum LogType {
-  INFO = "INFO",
-  ERROR = "ERROR",
-  FATAL = "FATAL",
-  DEBUG = "DEBUG"
+    INFO = "INFO",
+    ERROR = "ERROR",
+    FATAL = "FATAL",
+    DEBUG = "DEBUG"
 }
 export interface LoggerConfig {
-  debugEnable?: boolean;
-  logFile?: string;
+    debugEnable?: boolean;
+    logFile?: string;
 }
 
 export default Logger;
