1a2cbf89c39b57592de59ffc0fd9aa70b9b850bd
iuzziel <iuzziel@hotmail.fr>
2019-04-12 21:56:30 +0200

    Category added

--- src/logger.ts
+++ src/logger.ts
@@ -1,54 +1,47 @@
-import { appendFile } from "fs";
+import * as fs from "fs";
 
 export class Logger {
     private logFile: string = "undefined.log";
+    private logCategory: string[] = new Array<string>();
     private debugEnable: boolean = false;
     static _instance: Logger;
 
     constructor(loggerConfig?: LoggerConfig) {
-        if (Logger._instance) {
-            throw new Error("Singleton, utiliser Logger.getInstance()");
-        }
+        if (Logger._instance) throw new Error("Singleton, utiliser Logger.getInstance()");
+
         if (loggerConfig) {
-            if (loggerConfig.debugEnable) {
-                this.debugEnable = loggerConfig.debugEnable;
-            }
-            if (loggerConfig.logFile) {
-                this.logFile = loggerConfig.logFile;
-            }
+            if (loggerConfig.debugEnable) this.debugEnable = loggerConfig.debugEnable;
+            if (loggerConfig.logFile) this.logFile = loggerConfig.logFile;
+            if (loggerConfig.logCategory) this.logCategory = loggerConfig.logCategory;
         }
     }
 
-    private _log(logType: LogType, message: any): void {
-        let logMessage = JSON.stringify(message);
-        appendFile(this.logFile,
-            `[${new Date().toLocaleString()}][${logType}] ${logMessage}\r\n`, err => {
-                console.error(err);
-            }
-        );
-        if (logType == LogType.INFO || logType == LogType.DEBUG) {
-            console.log(logMessage);
-        } else if (logType == LogType.ERROR || logType == LogType.FATAL) {
-            console.error(logMessage);
-        }
+    private _log(logType: LogType, message: any, logCategory?: string): Promise<void> {
+        if (logCategory && this.logCategory
+            && Array.isArray(this.logCategory) && this.logCategory.length > 0
+            && this.logCategory.findIndex((v, i, a) => v === logCategory) !== -1)
+            return fs.promises.appendFile(this.logFile, `[${new Date().toLocaleString()}][${logType}][${logCategory}] ${JSON.stringify(message)}\r\n`)
+                .catch(err => console.error(err));
+        else
+            return fs.promises.appendFile(this.logFile, `[${new Date().toLocaleString()}][${logType}] ${JSON.stringify(message)}\r\n`)
+                .catch(err => console.error(err));
     }
 
-    public info(message: any): void {
-        this._log(LogType.INFO, message);
+    public info(message: any, logCategory?: string): Promise<void> {
+        return this._log(LogType.INFO, message, logCategory);
     }
 
-    public error(message: any): void {
-        this._log(LogType.ERROR, message);
+    public error(message: any, logCategory?: string): Promise<void> {
+        return this._log(LogType.ERROR, message, logCategory);
     }
 
-    public fatal(message: any): void {
-        this._log(LogType.FATAL, message);
+    public fatal(message: any, logCategory?: string): Promise<void> {
+        return this._log(LogType.FATAL, message, logCategory);
     }
 
-    public debug(message: any): void {
-        if (this.debugEnable) {
-            this._log(LogType.DEBUG, message);
-        }
+    public debug(message: any, logCategory?: string): Promise<void> {
+        if (this.debugEnable)
+            return this._log(LogType.DEBUG, message, logCategory);
     }
 
     public getLogFileName(): string {
@@ -60,9 +53,7 @@ export class Logger {
     }
 
     static getInstance(): Logger {
-        if (!Logger._instance) {
-            Logger._instance = new Logger();
-        }
+        if (!Logger._instance) Logger._instance = new Logger();
         return Logger._instance;
     }
 }
@@ -73,9 +64,11 @@ enum LogType {
     FATAL = "FATAL",
     DEBUG = "DEBUG"
 }
+
 export interface LoggerConfig {
     debugEnable?: boolean;
     logFile?: string;
+    logCategory?: string[];
 }
 
 export default Logger;
